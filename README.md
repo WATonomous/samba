6132b9fa30c29a0fc67bb97fe8eb435ffd83e65c
Ben Zhang <ben.zhang@watonomous.ca>
2021-05-27 14:44:51 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Samba
+
+This repo contains a simple script to run a samba server. See `run_samba.sh` for more details.
+
---
+++ run_samba.sh
@@ -0,0 +1,9 @@
+# share the current directory (public is readable by all connected users)
+# user:pass is watosamba:watosamba (read-write) or use guest access (readonly)
+
+# more documentation here: https://github.com/dperson/samba
+
+docker stop samba
+docker rm samba
+
+docker run -it --name samba -p 139:139 -p 445:445 -e USERID=$(id -u) -e GROUPID=$(id -g) -v $(pwd):/share -d dperson/samba -p -u "watosamba;watosamba" -s "public;/share/public;;;;;;watosamba;"
